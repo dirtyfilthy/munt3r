a032a0b9fc504b2808ae8b1657b9a9d349886260
Caleb <caleb.anderson@contextis.com>
2017-11-13 17:06:56 +1100

    initial

---
+++ README.md
@@ -0,0 +1,76 @@
+# munt3r.py: a HTTP to STOMP-over-websocket shim for pentesters
+
+## What does this do?
+
+munt3r converts HTTP requests you send to its built in HTTP server into STOMP-over-websocket requests which are forwarded upstream, and then converts the websocket response back into HTTP on the way return trip. 
+
+## What's STOMP?
+
+STOMP a protocol for pubsub and queue services like RabbitMQ, ActiveMQ etc. STOMP-over-websockets is exactly  what it says on the tin.  STOMP-over-websockets seems to be a fairly common way to implement a rich browser interface in the Spring world. STOMP requests typically look something like this:
+
+    SEND
+    destination:/app/xml.request
+    content-length:288
+    
+    {"requestDetails":{"id":"11bcc574-49e9-4e58-82a0-b1b0a001b2a5","subId":"5148966c-80b7-4403-b643-28a45be63dcd"},"xmlMessage":"<request id=\"af5dc512-5b28-4739-9371-3d389c40f19c:666666\" method=\"getClientLinked\" service=\"CDS\"><clcode>666666</clcode><linkType>AUTO</linkType></request>"}
+
+
+## What is the point of this?
+
+Testing websockets is kinda lame. The tools just don't support websockets very well, if they do at all. However, HTTP as a protocol is very well supported by a whole bunch of tools. munt3r lets you use your standard HTTP compatible tools to test STOMP-over-websocket. For instance, you can now use Burp repeater, activescan or intruder, wfuzz or sqlmap or anything else you like
+
+## How do I use this?
+
+### Requirements
+
+* python3
+* pip -r requirements.txt
+
+### Usage
+
+    usage: munt3r.py [-h] [--port PORT] [--cookie COOKIE] [--host HOST]
+                     [--subscribe SUB] [--verbose] URL. 
+
+* URL is the URL used to upgrade to the websocket and must start with the "ws://" schema (i.e. "ws://example.com:8080/ws"). 
+* --cookie is used to set an auth cookie, you should supply the full header value i.e. "JSESSIONID=xxxxxxx;Path=/;HttpOnly"
+* --port is the port the web server will start on the loopback interface (default 9090)
+* --host sets the Host and Origin headers
+* --subscribe subscribes to a topic upon connection. You will generally need to subscribe to a topic to get any responses
+* --verbose provides too much information
+
+To use this to test, get the websocket URL and auth  cookie using burp. As STOMP is a pubsub protocol, you will probably need to subscribe to a topic to get responses to your request. Subscribe  requests start with SUBSCRIBE as a single word on the first line and generally happen immediately after a CONNECT request. The topic is in the "destination" header:
+
+    SUBSCRIBE
+    id:sub0
+    destination:/user/queue/*
+
+Note that websockets (and STOMP) is not a synchronous request/reply protocol -- but munt3r pretends it is, and sometimes applications use it that way. munt3r will always return the first reply after you send your request, whatever it is. This may or may not be appropiate. The following provides a typical example of command line arguments:
+
+    python ./munt3r.py -v -p 9090 -H websocket-dev.local:65000 -s "/user/queue/*" -c "JSESSIONID=AC86749C01A1B5E1B0118A750717E61E;path=/;HttpOnly" ws://192.168.3.5:65000/ws
+
+After the server starts up, you can now make HTTP POST requests to the local web server and have them turned into STOMP-over-websockets SEND requests. The HTTP URI path will be converted into the STOMP destination header. 
+
+For example (burp proxy running 8080):
+
+    curl  http://127.0.0.1:9090/app/alerts --proxy http://127.0.0.1:8080 -d '{"search":"hello"}'
+
+Will be converted into the following STOMP request upstream:
+
+    SEND
+    destination:/app/alerts
+    content-length:18
+
+    {"search":"hello"}
+
+After the request is captured in burp you can the use all of burps tools. 
+
+
+
+
+
+
+
+
+
+
+
---
+++ munt3r.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import asyncio
+import websockets
+from aiohttp import web
+import argparse
+import sys
+
+DEFAULT_PORT=9090
+
+url     = None
+cookie  = None
+host    = None
+port    = DEFAULT_PORT
+verbose = False
+topic   = None
+
+
+def verbose_msg(msg):
+	global verbose
+	if not verbose:
+		return
+	print(msg)
+	sys.stdout.flush()
+
+
+def build_request(method, headers, body):
+	req = bytearray()
+	req.extend("{}\n".format(method).encode())
+	for k, v in headers.items():
+		req.extend("{}:{}\n".format(k, v).encode())
+	req.append(0x0a)
+	req.extend(body.encode())
+	req.append(0x00)
+	return bytes(req)
+
+
+def parse_body(raw_resp):
+	body_start = raw-resp.find("\n\n")+2
+	body_end   = raw_resp.find("{}".format(chr(0)))
+	body = raw_resp[body_start:body_end]
+	return body
+
+
+
+def send_request(websocket, method, headers={}, body="", expect_response=False, parse_body=True):
+	req = build_request(method, headers, body)
+	yield from websocket.send(req)
+	if expect_response:
+		resp = yield from websocket.recv()
+		if  parse_body:
+			resp = parse_body(resp)
+		return resp
+	return
+
+def subscribe(websocket, subid, dest):
+	send_request(websocket, "SUBSCRIBE", headers={"id":subid,"destination":dest})
+
+
+def disconnect(websocket):
+	send_request(websocket, "DISCONNECT")
+	websocket.close()
+
+
+
+@asyncio.coroutine
+def connect(url, cookie):
+	global host
+	global topic
+	headers = {}
+	if cookie is not None:
+		headers["Cookie"] = cookie
+	if host is not None:
+		headers["Host"] = host
+	#headers["X-Application-Context"]="application:uat"
+	ws = yield from websockets.connect(url, extra_headers=headers, compression=None)
+	verbose_msg("sending CONNECT")
+	send_request(ws, "CONNECT", {"accept-version":"1.1,1.2","heart-beat":"0,0"}, expect_response=True)
+	verbose_msg("subscribing to {}..".format(topic))
+	if topic:
+		subscribe(ws, subid="sub0", dest=topic)
+	verbose_msg("OK")
+	return ws
+
+@asyncio.coroutine
+def request(url, cookie, dest, body):
+	verbose_msg("connecting to {}".format(url))
+	ws = yield from connect(url, cookie)
+
+	verbose_msg("sending main...")
+	resp = send_request(ws, "SEND", {"destination":dest, "content-length":len(body)}, body, expect_response=True)
+	verbose_msg("sending main...")
+	disconnect(ws)
+	return body
+
+
+@asyncio.coroutine
+def handler(req):
+	global url
+	global cookie
+	path = req.path
+	data = yield from req.read()
+	print("handling request {}".format(path))
+	resp = yield from request(url, cookie, path, data)
+	
+	return web.Response(body=resp, content_type="text/json")
+
+@asyncio.coroutine
+def main_loop(loop):
+	global port
+	server = web.Server(handler)
+	print("spawning server on 127.0.0.1:{}".format(port))
+	yield from loop.create_server(server, "127.0.0.1", port)
+	yield from asyncio.sleep(100*3600)
+
+def start():
+	verbose_msg("started verbose")
+	loop = asyncio.get_event_loop()
+	try:
+		loop.run_until_complete(main_loop(loop))
+	except KeyboardInterrupt:
+		raise
+	loop.close()
+
+parser = argparse.ArgumentParser(description="HTTP to STOMP-over-websocket shim, 2017 Caleb Anderson")
+parser.add_argument('url', metavar='URL', type=str, nargs=1, help='web socket URL for initial handshake i.e. ws://server:1234/socket')
+parser.add_argument("--port","-p", dest="port", type=int, default=DEFAULT_PORT, help="start webserver on port (default {})".format(DEFAULT_PORT))
+parser.add_argument("--cookie","-c", dest="cookie", type=str, default=None, help="cookie for initial websocket handshake")
+parser.add_argument("--host","-H", dest="host", type=str, default=None, help="vhost for Host header")
+parser.add_argument("--subscribe","-s", dest="sub", type=str, default=None, help="subscribe to topic on connect")
+parser.add_argument("--verbose","-v", action="store_true")
+args = parser.parse_args()
+cookie  = args.cookie
+port    = args.port
+url     = args.url[0]
+verbose = args.verbose
+host    = args.host
+topic   = args.sub
+start()
+
+
+
+
+
+
+
+
